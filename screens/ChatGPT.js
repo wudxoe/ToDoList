0dff19a53de470f9d816692543d47fe4cfbe6937
wudxoe <120737105+wudxoe@users.noreply.github.com>
2023-12-11 10:43:18 +0900

    Create ChatGPT.js

---
+++ screens/ChatGPT.js
@@ -0,0 +1,17 @@
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet
+} from 'react-native'
+import OpenAI from '../components/OpenAI'
+import OpenAIGPT from '../components/OpenAIGPT'
+
+
+const ChatGPT = () => {
+  return (
+    <OpenAIGPT/>
+  )
+}
+
+export default ChatGPT
